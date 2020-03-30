d003c4aa41d070f848bf50347e9cdae0bee96890
RaissonSouto <raissonsouto@outlook.com>
2020-03-29 23:41:05 -0300

    ajeintando o scroll to

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,25 @@
-function scrolling(to){
+var sectionActive = "home"
+
+window.addEventListener('load',() => {
+    document.getElementById(sectionActive+"-header").style.color = "red";
+    document.getElementById(sectionActive+"-nav").style.backgroundColor = "green";
+});
+
+function scrolling(to){ 
 
     window.scrollTo(0, document.getElementById(to+'-main').offsetTop);
-    document.getElementById(to+'-nav').classList
 }
 
-document.getElementById("home-header").addEventListener("click", () => {scrolling("home-main")});
-document.getElementById("about-header").addEventListener("click", () => {scrolling("about-main")});
-document.getElementById("experience-header").addEventListener("click", () => {scrolling("experience-main")});
-document.getElementById("skills-header").addEventListener("click", () => {scrolling("skills-main")});
-document.getElementById("portifolio-header").addEventListener("click", () => {scrolling("portifolio-main")});
-document.getElementById("contact-header").addEventListener("click", () => {scrolling("contact-main")});
+document.getElementById("home-header").addEventListener("click", () => {scrolling("home")});
+document.getElementById("about-header").addEventListener("click", () => {scrolling("about")});
+document.getElementById("experience-header").addEventListener("click", () => {scrolling("experience")});
+document.getElementById("skills-header").addEventListener("click", () => {scrolling("skills")});
+document.getElementById("portifolio-header").addEventListener("click", () => {scrolling("portifolio")});
+document.getElementById("contact-header").addEventListener("click", () => {scrolling("contact")});
+
+document.getElementById("home-nav").addEventListener("click", () => {scrolling("home")});
+document.getElementById("about-nav").addEventListener("click", () => {scrolling("about")});
+document.getElementById("experience-nav").addEventListener("click", () => {scrolling("experience")});
+document.getElementById("skills-nav").addEventListener("click", () => {scrolling("skills")});
+document.getElementById("portifolio-nav").addEventListener("click", () => {scrolling("portifolio")});
+document.getElementById("contact-nav").addEventListener("click", () => {scrolling("contact")});
