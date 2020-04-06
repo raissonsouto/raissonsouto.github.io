eb51debd17af3fcd7983258a78587abd37926ab2
RaissonSouto <raissonsouto@outlook.com>
2020-04-06 02:09:02 -0300

    coloquei umas bolinhas do lado pra marcar em q parte do site a pessoa esta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,14 @@ var sectionActive = 'home';
 function scrollingTo(section) { 
 
   document.getElementById(sectionActive+"-header").querySelector('a').style.color = "#000";
-  document.getElementById(sectionActive+"-nav").style.backgroundColor = "#aaa";
 
   sectionActive = section;
 
   document.getElementById(section+"-header").querySelector('a').style.color = "#000aaa";
-  document.getElementById(section+"-nav").style.backgroundColor = "#000aaa";
+
+  if (window.innerWidth <= 720) {
+    toggleHamburguer();
+  }
 
 }
 
